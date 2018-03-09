85e22cfdfa1dd5ea22237427f05143898a8ed464
Saikumar5679 <35543689+Saikumar5679@users.noreply.github.com>
2018-03-09 14:13:08 -0600

    icp-8
    In class programming assignment-8

---
+++ source/CS5551-Tutorial-VIII/Learning Node/Client Server/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+            var searchQuery = document.getElementById("txt_searchFilter").value;
+            if (!(placeEntered != null && placeEntered != "" && searchQuery != null && searchQuery != "")) {
+                return;
+            }
+            var handler = $http.get("http://numbersapi.com/" + placeEntered + "/" + searchQuery + "/date?json");
+            handler.success(function (data) {
+
+
+                $scope.venueList[0] = {
+                    "name": data.text
+                };
+
+
+            })
+            handler.error(function (data) {
+                alert("There was some error processing your request. Please try after some time.");
+            });
+        }
